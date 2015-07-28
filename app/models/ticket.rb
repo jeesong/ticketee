87caf00c7a74c0aa086057d92b997343b05cbe40
jeesong <jeewsong@gmail.com>
2015-07-27 22:47:29 -0700

    Link tickets to author on creation

--- app/models/ticket.rb
+++ app/models/ticket.rb
@@ -1,5 +1,7 @@
 class Ticket < ActiveRecord::Base
   belongs_to :project
+  # since we don't have a model named author, use the class_name option helps point towards user model
+  belongs_to :author, class_name: "User"
 
   validates :title, presence: true
   validates :description, presence: true, length: { minimum: 10 }
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150728031846) do
+ActiveRecord::Schema.define(version: 20150728053245) do
 
   create_table "projects", force: true do |t|
     t.string   "name"
@@ -26,6 +26,7 @@ ActiveRecord::Schema.define(version: 20150728031846) do
     t.integer  "project_id"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "author_id"
   end
 
   add_index "tickets", ["project_id"], name: "index_tickets_on_project_id"
