ee230b4dedd75a6c7dc5e64e224604c4d9a537ec
jeesong <jeewsong@gmail.com>
2015-08-06 23:04:52 -0700

    restrict access to project actions to admins only

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,12 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  def require_signin!
+    if current_user.nil?
+      flash[:alert] = "You need to sign in or sign up before continuing."
+      redirect_to root_path
+    end
+  end
+  helper_method :require_signin!
 end
--- spec/controllers/projects_controller_spec.rb
+++ spec/controllers/projects_controller_spec.rb
@@ -1,12 +1,32 @@
+# require 'rails_helper'
+
+# RSpec.describe ProjectsController, :type => :controller do
+#   it "displays an error for a missing project" do 
+#     get :show, id: "not-here"
+
+#     expect(response).to redirect_to(projects_path)
+
+#     message = "The project you were looking for could not be found."
+#     expect(flash[:alert]).to eql(message)
+#   end
+# end
+
 require 'rails_helper'
 
-RSpec.describe ProjectsController, :type => :controller do
-  it "displays an error for a missing project" do 
-    get :show, id: "not-here"
+RSpec.describe ProjectsController do 
+  let(:user) { FactoryGirl.create(:user) }
 
-    expect(response).to redirect_to(projects_path)
+  context "standard users" do 
+    before do 
+      sign_in(user)
+    end
 
-    message = "The project you were looking for could not be found."
-    expect(flash[:alert]).to eql(message)
+    it "cannot access the new action" do 
+      # make a GET request to the new action of the projects controller
+      get :new 
+
+      expect(response).to redirect_to('/')
+      expect(flash[:alert]).to eql("You must be an admin to do that.")
+    end
   end
 end
